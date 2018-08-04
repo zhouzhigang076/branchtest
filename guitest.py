26adaa07166acea89463d1e2715aad6e78ba0e15
zhouzhigang <zhigang076@outlook.com>
2018-08-05 02:20:31 +0800

    branch test

---
+++ guitest.py
@@ -0,0 +1,17 @@
+print("test clone")
+print("hello world！！！")
+print("111111")
+print（"time.time()"）
+print("二次测试")
+print（"二次测试"）
+print（"四次测试"）
+print("三次测试1")
+print("测试啦啦啦啦")
+print（”三次测试“）
+print("今天要吃鸡")
+print("五次测试")
+print("北京的天太热辣")
+
+
+
+
